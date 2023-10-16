215893c37acd63835d974c56b5c3d65d7d810add
Halil-pgh <halil.pgh@gmail.com>
2023-10-16 17:46:43 +0300

    windows build added

---
+++ build/build-windows-ninja.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+:: Get the directory of the batch file
+set SOURCE_DIR=%~dp0..
+
+if "%1"=="debug" (
+    set CMAKE_BUILD_TYPE=Debug
+    set BUILD_DIR=%SOURCE_DIR%\cmake-build-debug
+) else (
+    set CMAKE_BUILD_TYPE=Release
+    set BUILD_DIR=%SOURCE_DIR%\cmake-build-release
+)
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=OFF -S %SOURCE_DIR% -B %BUILD_DIR%
+ninja -C %BUILD_DIR%
